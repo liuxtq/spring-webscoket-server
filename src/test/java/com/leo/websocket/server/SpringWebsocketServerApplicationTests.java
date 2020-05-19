e357c67baef58e71116beef353af1f288a2efc5d
liufeng <liu_xtq@163.com>
2020-05-19 16:03:51 +0800

    update

---
+++ src/test/java/com/leo/websocket/server/SpringWebsocketServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leo.websocket.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebsocketServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
